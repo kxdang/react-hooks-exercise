eb8b9ce52e524fef0dc80c060a8425ef89ef66ee
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-11 16:26:16 -0400

    changing to use hooks and toggle hook

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,24 +1,15 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: "english" };
-    this.changeLanguage = this.changeLanguage.bind(this);
-  }
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("english");
 
-  changeLanguage(e) {
-    this.setState({ language: e.target.value });
-  }
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
+  const changeLanguage = (e) => setLanguage(e.target.value);
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 }
